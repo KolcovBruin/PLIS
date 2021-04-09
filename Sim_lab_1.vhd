9ba68d3fc01e66370e6a6f4486b2105e39bb3423
KolcovBruin <47564050+KolcovBruin@users.noreply.github.com>
2021-04-09 18:07:20 +0300

    Add files via upload

---
+++ Sim_lab_1.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09.04.2021 14:04:17
+-- Design Name: 
+-- Module Name: Sim_lab_1 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Sim_lab_1 is
+    Port ( a : in STD_LOGIC;
+           b : in STD_LOGIC;
+           c : in STD_LOGIC;
+           d : out STD_LOGIC;
+           e : out STD_LOGIC);
+end Sim_lab_1;
+
+architecture Behavioral of Sim_lab_1 is
+
+begin
+
+
+end Behavioral;
